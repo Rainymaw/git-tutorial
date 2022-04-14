d95532a1397ea0c8d187728fe04bb216787d0c7b
Rainymaw <saidanis.khouider@outlook.fr>
2022-04-14 10:26:18 +0100

    implementing some basic authentication

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const validerForm = (e)=> {
-    e.preventDefault()
-    alert('Vous avez cliqué sur le formulaire')
+    e.preventDefault();
+    console.log(e);
+    var nom = document.form.nom.value;
+    if (nom ==="admin" ){
+        alert('Veuillez utiliser vos droits admin')
+    }else{
+        alert(nom)
+    }
+    var motdepasse = document.form.password.value
+    if (motdepasse.length <8){
+        alert('votre mot de passe est trop faible')
+    }else{
+        alert('mot de passe accepté')
+    }
+    console.log(motdepasse)
 }
